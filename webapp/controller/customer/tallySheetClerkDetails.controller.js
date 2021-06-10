21c37bd01fed8c3b39b144956c52bb412139dca2
jenngit <jeenlal2304@gmail.com>
2021-06-10 16:24:12 +0530

    tally sheet clerk - adding filter service for callsign

--- webapp/controller/customer/tallySheetClerkDetails.controller.js
+++ webapp/controller/customer/tallySheetClerkDetails.controller.js
@@ -12,6 +12,7 @@ sap.ui.define([
 			var id = oEvent.getParameter("arguments").id;
 			this.getUserName();
 			this.getView().byId(this.getView().getId() + "--tallyCargochangId").setVisible(false);
+			var that = this;
 			if (id !== 'false') {
 				this.getView().byId(this.getView().getId() + "--tallyClerkDispId").setVisible(true);
 				this.getView().byId(this.getView().getId() + "--tallyClerkchangId").setVisible(false);
@@ -21,7 +22,6 @@ sap.ui.define([
 				this.getView().byId(this.getView().getId() + "--create").setVisible(false);
 				var oModel = this.getOwnerComponent().getModel("s4Model");
 				oModel.setUseBatch(false);
-				var that = this;
 				oModel.read("/ClerkTallySheetSet('" + id + "')", {
 					urlParameters: {
 						"$expand": "ServiceChargeSet,CargoDetailsSet"
@@ -44,8 +44,43 @@ sap.ui.define([
 				this.getView().byId(this.getView().getId() + "--save").setVisible(false);
 				this.getView().byId(this.getView().getId() + "--create").setVisible(true);
 				this.getView().byId(this.getView().getId() + "--edit").setVisible(false);
+				setTimeout(function() {
+					var modData = that.getView().getModel("manifestListModel").getData();
+					that.getFilter(modData);
+				}, 2000);
 				sap.ui.core.BusyIndicator.hide();
 			}
+		},
+		getFilter: function(modData) {
+			var keys = ['CallSign'],
+				filtered = modData.filter(
+					(s => o =>
+						(k => !s.has(k) && s.add(k))
+						(keys.map(k => o[k]).join('|'))
+					)
+					(new Set)
+				);
+			var filteredData = filtered.filter(res => res.CallSign !== '');
+			this.getView().setModel(new JSONModel(filteredData), "callSignModel");
+		},
+		handleCallSignPress: function(evt) {
+			this.getView().byId("manifestSel").getSelectedKey() !== '' ? this.getView().byId("manifestSel").setSelectedKey(null) : ''
+			var oModel = this.getOwnerComponent().getModel("s4Model");
+			oModel.setUseBatch(false);
+			var that = this;
+			oModel.read("/ManifestListSet", {
+				urlParameters: {
+					"$filter": "ImStatus eq 'OPEN' and CallSign eq '" + evt.getSource().getSelectedKey() + "'"
+				},
+				success: function(data) {
+					that.getView().setModel(new JSONModel(data.results), "manifestFilterListModel");
+					sap.ui.core.BusyIndicator.hide();
+				},
+				error: function(oResponse) {
+					sap.m.MessageToast.show(oResponse.statusText);
+					sap.ui.core.BusyIndicator.hide();
+				}
+			});
 		},
 		addNewRowPress: function(evt) {
 			evt.getSource().getAlt() === "X" ? this.goToCargoDetailsSet() : this.goToServiceChargeSet(evt.getSource().getAlt());
@@ -53,18 +88,18 @@ sap.ui.define([
 		goToServiceChargeSet: function(item_type) {
 			this.getView().getModel("tSClerkDetailModel").getData().ServiceChargeSet.results.unshift({
 				"GUID": "",
-				"ItemNo": "",
-				"ItemType": item_type,
+				"ItmNo": "",
+				"ItmType": item_type,
 				"ManifestNo": "",
-				"Commodities": "",
-				"EquipmentCode": "",
+				"Commodity": "",
+				"Equipment": "",
 				"DelType": "",
-				"NoOfPackage": "",
+				"NoOfPacks": "",
 				"Shift": "",
 				"HatchNo": "",
 				"Date": "",
-				"FromTime": "",
-				"ToTime": "",
+				"From": "",
+				"To": "",
 				"Weight": "",
 				"Quantity": "",
 				"UOM": "",
@@ -119,7 +154,7 @@ sap.ui.define([
 			var that = this;
 			oModel.create("/ClerkTallySheetSet", oEntry, {
 				success: function(data) {
-						sap.m.MessageToast.show("Tally Sheet with Guid Number - " + data.GUID + " Created Successfully", {
+					sap.m.MessageToast.show("Tally Sheet with Guid Number - " + data.GUID + " Created Successfully", {
 						closeOnBrowserNavigation: false
 					});
 					that.getRouter().navTo("tallySheetClerkList");
@@ -133,10 +168,9 @@ sap.ui.define([
 		},
 		handleMDetailsPress: function(evt) {
 			var oEntry = this.getView().getModel("tSClerkDetailModel").getData();
-			var res = Object.keys(oEntry).length === 0 && oEntry.constructor === Object;
-			var model = evt.getSource().getModel("manifestListModel");
+			var model = evt.getSource().getModel("manifestFilterListModel");
 			var oItem = evt.getSource().getSelectedItem();
-			var oContext = oItem.getBindingContext("manifestListModel");
+			var oContext = oItem.getBindingContext("manifestFilterListModel");
 			var oPath = oContext.getPath();
 			var obj = model.getProperty(oPath);
 			var today = new Date();
@@ -148,34 +182,41 @@ sap.ui.define([
 			var date = yyyy + '' + mmFormat + '' + ddFormat;
 			var oData = {
 				ActionFlag: "",
-				AgentCode: obj.AgentCode ? obj.AgentCode : '',
+				AgentCode: obj.AgentID ? obj.AgentID : '',
 				AgentName: obj.AgentName,
-				Berth: "",
+				Berth: oEntry['ManifestNo'] ? oEntry.Berth : '',
 				CallSign: obj.CallSign,
 				CargoDetailsSet: {
-					results: res === false ? oEntry.CargoDetailsSet.results : []
+					results: oEntry['ManifestNo'] ? oEntry.CargoDetailsSet.results : []
 				},
 				CustomsRefNo: obj.CustomsRefManifestNo,
 				Date: date,
 				Derrik_Crain: "",
 				EmployeeId: "",
 				GUID: "",
-				HatchNo: "",
+				HatchNo: oEntry['ManifestNo'] ? oEntry.HatchNo : '',
 				IMO: obj.IMOnumber,
 				ImFlag: obj.ImFlag,
 				Import_Export: "",
 				ManifestNo: obj.ManifestNo,
-				NoOfHooks: "",
+				NoOfHooks: oEntry['ManifestNo'] ? oEntry.NoOfHooks : '',
 				ServiceChargeSet: {
-					results: res === false ? oEntry.ServiceChargeSet.results : []
+					results: oEntry['ManifestNo'] ? oEntry.ServiceChargeSet.results : []
 				},
-				Shift: "",
+				Shift: oEntry['ManifestNo'] !== '' ? oEntry.Shift : '',
 				TallyServiceNo: "",
-				TimeCommenced: "",
+				TimeCommenced: oEntry['ManifestNo'] ? oEntry.TimeCommenced : '',
 				TimeCompleted: "",
 				VesselName: obj.VesselName
 			};
 			this.getView().setModel(new JSONModel(oData), "tSClerkDetailModel");
+		},
+		deleteThisItem: function(oEvent) {
+			var oRow = oEvent.getSource().getParent(); //Get Row
+            var iRowIndex = oRow.getIndex();
+            var oData = this.getView().getModel("tSClerkDetailModel").getData().ServiceChargeSet.results;
+            oData.splice(iRowIndex, 1);
+            this.getView().getModel("tSClerkDetailModel").refresh();
 		}
 	});
 });
